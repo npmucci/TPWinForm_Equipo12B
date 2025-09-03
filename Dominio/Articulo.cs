9d85842dc0d4ab50de44bc7ba5c78feaf03a6abc
npmucci <npmucci@gmail.com>
2025-09-03 09:13:10 -0300

    creacion de proyecto base y clases de dominio

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace Dominio
+{
+
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public decimal Precio { get; set; }
+        public List<Imagen> Imagenes { get; set; }
+
+
+    }
+}
